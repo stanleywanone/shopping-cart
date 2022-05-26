415b045040d9e41b2e70599d7ef52d5ffb68c7e8
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2022-05-25 22:34:29 -0500

    add select function in the reducers in itemSlice

--- src/store/itemsSlice.js
+++ src/store/itemsSlice.js
@@ -5,7 +5,7 @@ const itemsSlice = createSlice({
   initialState: { items: [], balance: 0, count: 0 },
   reducers: {
     add(state, action) {
-      state.items.push(action.payload)
+      state.items.push({ ...action.payload, number: 1 })
       state.balance += action.payload.price
       state.count += 1
     },
@@ -14,9 +14,25 @@ const itemsSlice = createSlice({
       state.balance -= action.payload.price
       state.count -= 1
     },
+    selectNumber(state, action) {
+      state.items = state.items.map((item) => {
+        if (item.name === action.payload.name) {
+          return {
+            ...item,
+            number: parseInt(action.payload.number),
+          }
+        }
+        return item
+      })
+      let newBlance = 0
+      state.items.forEach((item) => {
+        newBlance += item.price * item.number
+      })
+      state.balance = newBlance
+    },
   },
 })
 
-export const { add, remove } = itemsSlice.actions
+export const { add, remove, selectNumber } = itemsSlice.actions
 
 export default itemsSlice.reducer
